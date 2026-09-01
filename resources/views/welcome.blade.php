6ef06412deb8bae57cf1af87e037b8784c12627a
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2026-09-01 23:46:24 +0200

    fix: replace broken camelCase color classes with hyphenated variants
    - Register submit button: bg-primaryText / text-backgroundDark ->
      bg-primary-text / text-background-dark so the dark-mode toggle works.
    - Body pages: drop the redundant dark:bg-backgroundDark now that the
      prefers-color-scheme media query swaps --theme-background.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="bg-background dark:bg-backgroundDark text-text min-h-screen flex flex-col">
+    <body class="bg-background text-text min-h-screen flex flex-col">
         @include('partials.header')
 
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
